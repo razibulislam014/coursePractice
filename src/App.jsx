b3158f90fe74be9efe78748aaae9015301342030
MD Razibul <razibulislam665@gmail.com>
2026-02-17 07:13:00 +0600

    learned about key and rendering a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import Avatar from "./components/Avatar";
-import Profile from "./components/Profile";
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
 
 function App() {
+  const chemists = people.filter((person) => person.profession === "chemist");
+  const listItems = chemists.map((chemist) => <li key={chemist.id}>{chemist.name}</li>);
   return (
     <div>
-      <div>
-        <Profile>
-          <Avatar/>
-        </Profile>
-      </div>
+      <div>{listItems}</div>
     </div>
   );
 }
