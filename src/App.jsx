ede6a206d6a0ff3361aa6a0d06617fddf7348a54
MD Razibul <razibulislam665@gmail.com>
2026-03-07 11:19:39 +0600

    state as a memory finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,21 @@
-const people = [
-  {
-    id: 0,
-    name: "Creola Katherine Johnson",
-    profession: "mathematician",
-  },
-  {
-    id: 1,
-    name: "Mario José Molina-Pasquel Henríquez",
-    profession: "chemist",
-  },
-  {
-    id: 2,
-    name: "Mohammad Abdus Salam",
-    profession: "physicist",
-  },
-  {
-    id: 3,
-    name: "Percy Lavon Julian",
-    profession: "chemist",
-  },
-  {
-    id: 4,
-    name: "Subrahmanyan Chandrasekhar",
-    profession: "astrophysicist",
-  },
-];
+export default function App() {
+  return (
+    <Toolbar
+      onPlayMovie={() => alert("Playing!")}
+      onUploadImage={() => alert("Uploading!")}
+    />
+  );
+}
 
-function App() {
-  const chemists = people.filter((person) => person.profession === "chemist");
-  const listItems = chemists.map((chemist) => <li key={chemist.id}>{chemist.name}</li>);
+function Toolbar({ onPlayMovie, onUploadImage }) {
   return (
     <div>
-      <div>{listItems}</div>
+      <Button onSmash={onPlayMovie}>Play Movie</Button>
+      <Button onSmash={onUploadImage}>Upload Image</Button>
     </div>
   );
 }
 
-export default App;
+function Button({ onSmash, children }) {
+  return <button onClick={onSmash}>{children}</button>;
+}
